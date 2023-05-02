3f82ee2942259e09687919bac0b0eeb2cc4407d4
shreya <shettarshreya@gmail.com>
2023-05-02 10:58:14 +0530

    added dice_simulator.py

---
+++ dice_simulator.py
@@ -0,0 +1,21 @@
+import random
+z="y"
+
+#print(x)
+#z=input("Enter \"y\" or \"n\" to roll ")
+while z=="y":
+    x = random.randint(1,7)
+    if x== 1:
+        print(x)
+    if x==2:
+        print(x)
+    if x==3:
+        print(x)
+    if x==4:
+        print(x)
+    if x==5:
+        print(x)
+    if x==6:
+        print(x)
+    z=input("Enter \"y\" to roll ")
+        
